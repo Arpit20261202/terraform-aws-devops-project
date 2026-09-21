9301d08ca5a59c0925a578a1c5de3a7bd0d3f552
Arpit20261202 <eeearpit2015@gmail.com>
2026-09-21 11:41:55 +0000

    Fix GitHub Actions OIDC trust policy

---
+++ github-actions-role.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "github_actions" {
+  name = "terraform-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:Arpit20261202/terraform-aws-devops-project:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name      = "terraform-github-actions-role"
+    ManagedBy = "Terraform"
+  }
+}
