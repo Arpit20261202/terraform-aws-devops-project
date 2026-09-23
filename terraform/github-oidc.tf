3bd64fb7fcdd2e0ddacfe175afd6942ce8276172
Arpit20261202 <eeearpit2015@gmail.com>
2026-09-23 05:57:27 +0000

    Manage GitHub Actions OIDC IAM with Terraform

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -9,41 +9,42 @@ resource "aws_iam_openid_connect_provider" "github" {
 }
 
 resource "aws_iam_role" "github_actions" {
-  name = "${var.project_name}-github-actions-role"
+  name = "terraform-aws-devops-github-actions-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
 
-    Statement = [{
-      Effect = "Allow"
+    Statement = [
+      {
+        Effect = "Allow"
 
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
-      }
-
-      Action = "sts:AssumeRoleWithWebIdentity"
-
-      Condition = {
-        StringEquals = {
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
 
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+
+            "token.actions.githubusercontent.com:sub" = "repo:Arpit20261202@319696986/terraform-aws-devops-project@1379560845:ref:refs/heads/main"
+          }
         }
       }
-    }]
+    ]
   })
 }
+
 resource "aws_iam_role_policy" "github_actions" {
-  name = "${var.project_name}-github-actions-policy"
+  name = "terraform-aws-devops-github-actions-policy"
+
   role = aws_iam_role.github_actions.id
 
   policy = jsonencode({
     Version = "2012-10-17"
 
     Statement = [
-
       {
         Effect = "Allow"
 
@@ -65,7 +66,27 @@ resource "aws_iam_role_policy" "github_actions" {
           "ecr:UploadLayerPart"
         ]
 
-        Resource = aws_ecr_repository.app.arn
+        Resource = "arn:aws:ecr:ap-south-1:599657398123:repository/terraform-aws-devops-app"
+      },
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ec2:DescribeImages",
+          "ec2:DescribeInstances",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeRouteTables",
+          "ec2:DescribeInternetGateways",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeNetworkInterfaces"
+        ]
+
+        Resource = "*"
       }
     ]
   })
