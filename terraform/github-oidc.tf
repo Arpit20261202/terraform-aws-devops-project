3535c55ef4923376cdbca1dd3360112d2d82e3d7
Arpit20261202 <eeearpit2015@gmail.com>
2026-09-23 06:27:53 +0000

    Update CI-CD IAM permissions

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -1,5 +1,6 @@
 data "aws_caller_identity" "current" {}
 
+# GitHub OIDC Provider
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
@@ -8,6 +9,7 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
 }
 
+# GitHub Actions IAM Role
 resource "aws_iam_role" "github_actions" {
   name = "terraform-aws-devops-github-actions-role"
 
@@ -36,6 +38,7 @@ resource "aws_iam_role" "github_actions" {
   })
 }
 
+# Permissions for GitHub Actions
 resource "aws_iam_role_policy" "github_actions" {
   name = "terraform-aws-devops-github-actions-policy"
 
@@ -45,6 +48,8 @@ resource "aws_iam_role_policy" "github_actions" {
     Version = "2012-10-17"
 
     Statement = [
+
+      # ECR
       {
         Effect = "Allow"
 
@@ -59,6 +64,9 @@ resource "aws_iam_role_policy" "github_actions" {
         Effect = "Allow"
 
         Action = [
+          "ecr:CreateRepository",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
           "ecr:BatchCheckLayerAvailability",
           "ecr:CompleteLayerUpload",
           "ecr:InitiateLayerUpload",
@@ -69,25 +77,115 @@ resource "aws_iam_role_policy" "github_actions" {
         Resource = "arn:aws:ecr:ap-south-1:599657398123:repository/terraform-aws-devops-app"
       },
 
+      # VPC / EC2
       {
         Effect = "Allow"
 
         Action = [
+          "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:ModifyVpcAttribute",
+          "ec2:DescribeVpcs",
+
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:ModifySubnetAttribute",
+          "ec2:DescribeSubnets",
+
+          "ec2:CreateInternetGateway",
+          "ec2:DeleteInternetGateway",
+          "ec2:AttachInternetGateway",
+          "ec2:DetachInternetGateway",
+          "ec2:DescribeInternetGateways",
+
+          "ec2:CreateRouteTable",
+          "ec2:DeleteRouteTable",
+          "ec2:CreateRoute",
+          "ec2:DeleteRoute",
+          "ec2:AssociateRouteTable",
+          "ec2:DisassociateRouteTable",
+          "ec2:DescribeRouteTables",
+
+          "ec2:CreateTags",
+          "ec2:DeleteTags",
+          "ec2:DescribeTags",
+
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeSecurityGroups",
           "ec2:DescribeImages",
           "ec2:DescribeInstances",
-          "ec2:DescribeVpcs",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeRouteTables",
-          "ec2:DescribeInternetGateways",
-          "ec2:DescribeAvailabilityZones",
-          "ec2:DescribeTags",
           "ec2:DescribeVolumes",
           "ec2:DescribeNetworkInterfaces"
         ]
 
         Resource = "*"
+      },
+
+      # EKS
+      {
+        Effect = "Allow"
+
+        Action = [
+          "eks:CreateCluster",
+          "eks:DescribeCluster",
+          "eks:UpdateClusterConfig",
+          "eks:UpdateClusterVersion",
+          "eks:DeleteCluster",
+
+          "eks:CreateNodegroup",
+          "eks:DescribeNodegroup",
+          "eks:UpdateNodegroupConfig",
+          "eks:UpdateNodegroupVersion",
+          "eks:DeleteNodegroup",
+
+          "eks:ListClusters",
+          "eks:ListNodegroups",
+          "eks:TagResource",
+          "eks:UntagResource"
+        ]
+
+        Resource = "*"
+      },
+
+      # IAM
+      {
+        Effect = "Allow"
+
+        Action = [
+          "iam:GetRole",
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:UpdateRole",
+          "iam:PassRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PutRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies"
+        ]
+
+        Resource = [
+          "arn:aws:iam::599657398123:role/terraform-aws-devops-*"
+        ]
+      },
+
+      # GitHub OIDC Provider
+      {
+        Effect = "Allow"
+
+        Action = [
+          "iam:GetOpenIDConnectProvider",
+          "iam:CreateOpenIDConnectProvider",
+          "iam:DeleteOpenIDConnectProvider",
+          "iam:UpdateOpenIDConnectProviderThumbprint"
+        ]
+
+        Resource = "arn:aws:iam::599657398123:oidc-provider/token.actions.githubusercontent.com"
       }
     ]
   })
 }
+
+
